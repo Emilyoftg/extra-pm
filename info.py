087e4c8ff624000d8ee0c6f43433886c366e4e45
Emily <71280699+Emilyoftg@users.noreply.github.com>
2022-03-22 19:28:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,10 +28,7 @@ DATABASE_NAME = environ['DATABASE_NAME']
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
-default_start_msg = """
-**Hi, I'm Media Search Bot or ypu can call me as Auto-Filter Bot**
-Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
-"""
+default_start_msg = """ """
 START_MSG = environ.get('START_MSG', default_start_msg)
 
 BUTTON = environ.get("BUTTON",False)
